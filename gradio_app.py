5c48f7b145ef999289b0b5596f29226815d7f6bd
wwdok <wade.wang96@outlook.com>
2023-09-21 10:40:13 +0800

    update

--- gradio_app.py
+++ gradio_app.py
@@ -20,44 +20,7 @@ def ref_video_fn(path_of_ref_video):
     else:
         return gr.update(value=False)
 
-def download_model():
-    REPO_ID = 'vinthony/SadTalker-V002rc'
-    snapshot_download(repo_id=REPO_ID, local_dir='./checkpoints', local_dir_use_symlinks=True)
-
-def get_image_from_inference_tab(num):
-    # the same to `def deal_history` function
-    if not uuid:
-        if os.getenv("MODELSCOPE_ENVIRONMENT") == 'studio':
-            return "请登陆后使用! (Please login first)"
-        else:
-            uuid = 'qw'
-    
-    # if base_model_index is None:
-    #     raise gr.Error('请选择基模型(Please select the base model)!')
-    # if user_model is None:
-    #     raise gr.Error('请选择人物lora(Please select the character lora)!')    
-    # if lora_choice is None:
-    #     raise gr.Error('请选择LoRa文件(Please select the LoRa file)!')
-    # if style_model is None and lora_choice == 'preset':
-    #     raise gr.Error('请选择风格(Please select the style)!')
-    
-    # base_model = base_models[base_model_index]['model_id']
-    # style_model = styles[style_model]['name']
-    # save_dir = os.path.join('/tmp', uuid, 'inference_result', base_model, user_model)
-    # if lora_choice == 'preset':
-    #     save_dir = os.path.join(save_dir, 'style_' + style_model)
-    # else:
-    #     save_dir = os.path.join(save_dir, 'lora_' + os.path.basename(lora_choice).split('.')[0])
-    
-    # if not os.path.exists(save_dir):
-    #     return gr.Gallery.update(value=[], visible=True), gr.Gallery.update(value=[], visible=True)
-    
-    # img_path = os.path.join(save_dir, 'single', str(num) + '.png')
-    # if os.path.exists(img_path):
-    #     return img_path
-
 def sadtalker_demo(checkpoint_path='checkpoints', config_path='src/config', warpfn=None):
-    # download_model()
     sad_talker = SadTalker(checkpoint_path, config_path, lazy_load=True)
     with gr.Blocks(analytics_enabled=False) as sadtalker_interface:
         gr.Markdown("<div align='center'> <h2> 😭 SadTalker: Learning Realistic 3D Motion Coefficients for Stylized Audio-Driven Single Image Talking Face Animation (CVPR 2023) </span> </h2> \
@@ -71,11 +34,6 @@ def sadtalker_demo(checkpoint_path='checkpoints', config_path='src/config', warp
                     with gr.TabItem('Upload image'):
                         with gr.Row():
                             source_image = gr.Image(label="Source image", source="upload", type="filepath", elem_id="img2img_image").style(width=512)
-                        with gr.Row():
-                            gr.Markdown("Pick the image number:")
-                            num = gr.Number(value=0, show_label=False)
-                            img_btn = gr.Button('Load image from inference tab', variant='primary')
-                            img_btn.click(fn=get_image_from_inference_tab, inputs=[num], outputs=[source_image])
                 with gr.Tabs(elem_id="sadtalker_driven_audio"):
                     with gr.TabItem('Upload OR TTS'):
                         with gr.Column(variant='panel'):
@@ -98,8 +56,6 @@ def sadtalker_demo(checkpoint_path='checkpoints', config_path='src/config', warp
                         gr.Markdown("need help? please visit our [best practice page](https://github.com/OpenTalker/SadTalker/blob/main/docs/best_practice.md) for more detials")
                         with gr.Column(variant='panel'):
                         # with gr.Accordion("高级选项(Advanced Options)", open=False):
-                            # width = gr.Slider(minimum=64, elem_id="img2img_width", maximum=2048, step=8, label="Manually Crop Width", value=512) # img2img_width
-                            # height = gr.Slider(minimum=64, elem_id="img2img_height", maximum=2048, step=8, label="Manually Crop Height", value=512) # img2img_width
                             pose_style = gr.Slider(minimum=0, maximum=45, step=1, label="Pose style", value=0) # 
                             exp_weight = gr.Slider(minimum=0.5, maximum=2, step=0.1, label="expression scale", value=1)
                             size_of_image = gr.Radio([256, 512], value=256, label='face model resolution', info="use 256/512 model?") # 
@@ -158,7 +114,6 @@ def sadtalker_demo(checkpoint_path='checkpoints', config_path='src/config', warp
  
 
 if __name__ == "__main__":
-
     demo = sadtalker_demo()
     demo.queue()
     demo.launch()
